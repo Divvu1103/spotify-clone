61a726eba3a1b89d9d4694d9a7af67aa63cda6e1
Divvu1103 <200110075@hbtu.ac.in>
2021-12-09 17:49:28 +0530

    spotify-clone

---
+++ spotifyclonneorignal/script.js
@@ -0,0 +1,169 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio("songs/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let masterSongName = document.getElementById("masterSongName");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
+
+let songs = [
+  {
+    songName: "Channa Mereya - Arjit singh",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "Mann Bharryaa 2.0 - B Praak",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
+  },
+  {
+    songName: "Ranjha-B Praak",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
+  },
+  {
+    songName: " Sapna -Arijit Singh",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jpg",
+  },
+  {
+    songName: "Bairaagi - Arjit Singh",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/5.jpg",
+  },
+  {
+    songName: "Phir Bhi Tumko Chaahunga - Arjit Singh",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/6.jpg",
+  },
+  {
+    songName: " Lambiyaan Si Judaiyaan - Arjit Singh",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/7.jfif",
+  },
+  {
+    songName: "Mast Magan - Arjit Singh",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/8.jfif",
+  },
+  {
+    songName: "Kabira - Arjit Singh",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/9.jfif",
+  },
+  {
+    songName: "Chunar - Arjit Singh ",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/10.jfif",
+  },
+];
+
+songItems.forEach((element, i) => {
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
+
+// Handle play/pause click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
+});
+// Listen to Events
+audioElement.addEventListener("timeupdate", () => {
+  // Update Seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+
+  let currenttime = audioElement.currentTime;
+  let cminutes = parseInt(currenttime / 60);
+
+  let cseconds = parseInt(-cminutes * 60 + currenttime);
+
+  let duration = audioElement.duration;
+  let dminutes = parseInt(duration / 60);
+
+  let dseconds = parseInt(-dminutes * 60 + duration);
+  let ctime;
+
+  if (cseconds < 10) {
+    ctime = cminutes + ":" + "0" + cseconds;
+  } else {
+    ctime = cminutes + ":" + cseconds;
+  }
+  let currentimeplayer = getElementById("currenttime");
+  currentimeplayer.innerhtml = ctime;
+
+  myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
+
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+    (element) => {
+      element.classList.remove("fa-pause-circle");
+      element.classList.add("fa-play-circle");
+    }
+  );
+};
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      makeAllPlays();
+      songIndex = parseInt(e.target.id);
+      e.target.classList.remove("fa-play-circle");
+      e.target.classList.add("fa-pause-circle");
+      audioElement.src = `songs/${songIndex + 1}.mp3`;
+      masterSongName.innerText = songs[songIndex].songName;
+      audioElement.currentTime = 0;
+      audioElement.play();
+      gif.style.opacity = 1;
+      masterPlay.classList.remove("fa-play-circle");
+      masterPlay.classList.add("fa-pause-circle");
+    });
+  }
+);
+
+document.getElementById("next").addEventListener("click", () => {
+  if (songIndex >= 9) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
+
+document.getElementById("previous").addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 0;
+  } else {
+    songIndex -= 1;
+  }
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
